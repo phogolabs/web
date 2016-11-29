1050dc86437108f4bb3f173d124b7bf5ddf52ba5
Svett Ralchev <svet@ralch.com>
2016-11-29 15:57:27 +0000

    Simplify the UI to match the company size

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,15 +2,7 @@ module Phogo
   module Controllers
     class MainController < ApplicationController
       get '/' do
-        erb :about, :layout => :'layout/_main'
-      end
-
-      get '/about' do
-        erb :about, :layout => :'layout/_main'
-      end
-
-      get '/approach' do
-        erb :approach, :layout => :'layout/_main'
+        erb :index, :layout => :'layout/_main'
       end
     end
   end
