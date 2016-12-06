431fdf868dd71caa7afa3ee81118a08d319473bf
Svett Ralchev <svet@ralch.com>
2016-12-06 14:13:00 +0000

    Decode the contact email to prevent spam

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,5 +13,16 @@ module Phogo
       options = 'class="active"' if current_page?(path)
       "<a href=\"#{path}\" #{options}>#{name}</a>"
     end
+
+    def mail_to(email_address)
+      email_address_encoded = 'mailto:'.unpack('C*').map { |c|
+        sprintf("&#%d;", c)
+      }.join + email_address.unpack('C*').map { |c|
+        symbol = c.chr
+        symbol =~ /\w/ ? sprintf("%%%x", c) : symbol
+      }.join
+      email_address_text = email_address.gsub(/@/, '<i class="fa fa-at"></i>')
+      "<a href=\"#{email_address_encoded}\">#{email_address_text}</a>"
+    end
   end
 end
