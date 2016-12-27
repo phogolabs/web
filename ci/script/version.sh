1b8fad56ad455743335f1a29fd7c706e82ec40a0
Svett Ralchev <svet@ralch.com>
2016-12-27 15:52:08 +0000

    Fix version task to create the output in correct location

--- ci/script/version.sh
+++ ci/script/version.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+VERSION_DIR="$(cd version && pwd)"
+
+# shellcheck disable=2164
 cd repository
 
-git tag --list 'v*' --contains HEAD | sed -n 's/^v//p' > version/number
-git rev-parse --short HEAD > version/hash
+git tag --list 'v*' --contains HEAD | sed -n 's/^v//p' > "$VERSION_DIR/number"
+git rev-parse --short HEAD > "$VERSION_DIR/hash"
