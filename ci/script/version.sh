8ea393e69da6688221f81158505fa32ff2aac530
Svett Ralchev <svet@ralch.com>
2016-12-28 19:28:10 +0000

    Use correct name for environment variables

--- ci/script/version.sh
+++ ci/script/version.sh
@@ -2,10 +2,10 @@
 
 VERSION_DIR="$(cd version && pwd)"
 
+[ -z "$PRODUCT_NAME" ] && echo "$PRODUCT_NAME" > "$VERSION_DIR/name"
+
 # shellcheck disable=2164
 cd repository
 
 git tag --list 'v*' --contains HEAD | sed -n 's/^v//p' > "$VERSION_DIR/number"
 git rev-parse --short HEAD > "$VERSION_DIR/hash"
-
-[ -z "$product_name" ] && echo "$product_name" > "$VERSION_DIR/name"
