c687a628fd279270ca6204cb008029904f308233
Svett Ralchev <svet@ralch.com>
2016-12-28 18:49:54 +0000

    Create GitHub release as part of creating final release

--- ci/script/version.sh
+++ ci/script/version.sh
@@ -7,3 +7,5 @@ cd repository
 
 git tag --list 'v*' --contains HEAD | sed -n 's/^v//p' > "$VERSION_DIR/number"
 git rev-parse --short HEAD > "$VERSION_DIR/hash"
+
+[ -z "$product_name" ] && echo "$product_name" > "$VERSION_DIR/name"
